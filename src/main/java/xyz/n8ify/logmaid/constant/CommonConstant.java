6ed8b331258211ac5fda66f548a93eefbd9dd14c
nata8ify <nata8ify@gmail.com>
2023-07-30 15:00:05 +0700

    --checkpoint v1.2

--- src/main/java/xyz/n8ify/logmaid/constant/CommonConstant.java
+++ src/main/java/xyz/n8ify/logmaid/constant/CommonConstant.java
@@ -3,8 +3,8 @@ package xyz.n8ify.logmaid.constant;
 public class CommonConstant {
 
     public static final String APP_NAME = "Logmaid";
-    public static final String VERSION = "1.1";
-    public static final String RELEASE_DATE = "08/05/2023";
+    public static final String VERSION = "1.2";
+    public static final String RELEASE_DATE = "30/07/2023";
 
     public static final String DEFAULT_OUTPUT_PATH_SUFFIX = "extractOut";
 
