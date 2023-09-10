4ab917b9cfb5488a050932341ab17ac07a65d832
nata6ify <nata6ify@gmail.com>
2023-09-10 10:19:42 +0700

    [grouped-keyword] modify label

--- src/main/java/xyz/n8ify/logmaid/constant/LabelConstant.java
+++ src/main/java/xyz/n8ify/logmaid/constant/LabelConstant.java
@@ -8,7 +8,7 @@ public class LabelConstant {
     public static final String INTERESTED_KEYWORD_TAB_LABEL = "Interested Keyword(s)";
     public static final String ADHOC_KEYWORD_TAB_LABEL = "Adhoc Keyword(s)";
     public static final String IGNORED_KEYWORD_TAB_LABEL = "Ignored Keyword(s)";
-    public static final String GROUPED_THREAD_TAB_LABEL = "Grouped Thread Keyword(s)";
+    public static final String GROUPED_THREAD_TAB_LABEL = "Grouped Keyword(s)";
     public static final String INTERESTED_KEYWORD_INPUT_HINT = "List of Interested Keyword(s)... (Split with new line)";
     public static final String ADHOC_KEYWORD_INPUT_HINT = "List of Adhoc Keyword(s)... (Split with new line)";
     public static final String IGNORED_KEYWORD_INPUT_HINT = "List of Ignored Keyword(s)... (Split with new line)";
