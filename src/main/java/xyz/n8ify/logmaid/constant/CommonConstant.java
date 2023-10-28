13642dadffd02cc057cf9effddb13f88ca6763e2
nata8ify <nata8ify@gmail.com>
2023-10-28 20:34:51 +0700

    --checkpoint v1.4, apply backgroup log extractor process

--- src/main/java/xyz/n8ify/logmaid/constant/CommonConstant.java
+++ src/main/java/xyz/n8ify/logmaid/constant/CommonConstant.java
@@ -3,8 +3,8 @@ package xyz.n8ify.logmaid.constant;
 public class CommonConstant {
 
     public static final String APP_NAME = "Logmaid";
-    public static final String VERSION = "1.3";
-    public static final String RELEASE_DATE = "30/07/2023";
+    public static final String VERSION = "1.4";
+    public static final String RELEASE_DATE = "28/10/2023";
 
     public static final String DEFAULT_OUTPUT_PATH_SUFFIX = "extractOut";
 
