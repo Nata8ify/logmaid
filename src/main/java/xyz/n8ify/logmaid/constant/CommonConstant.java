282f14cd949ca84b908badd305f97ca61283ecc2
nata6ify <nata6ify@gmail.com>
2024-05-14 16:05:24 +0700

    --checkpoint v1.4, apply default output destination setter

--- src/main/java/xyz/n8ify/logmaid/constant/CommonConstant.java
+++ src/main/java/xyz/n8ify/logmaid/constant/CommonConstant.java
@@ -3,8 +3,8 @@ package xyz.n8ify.logmaid.constant;
 public class CommonConstant {
 
     public static final String APP_NAME = "Logmaid";
-    public static final String VERSION = "1.4";
-    public static final String RELEASE_DATE = "28/10/2023";
+    public static final String VERSION = "1.5";
+    public static final String RELEASE_DATE = "14/05/2024";
 
     public static final String DEFAULT_OUTPUT_PATH_SUFFIX = "extractOut";
 
